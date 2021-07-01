e6bc955b3fd7992763886a06babb976a5552a312
Mattia <40793377+MattiaG96@users.noreply.github.com>
2021-07-01 10:24:20 +0100

    Initial Commit

---
+++ GraphQLCountry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphQLCountry
+//
+//  Created by Mattia on 1/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphQLCountry/GraphQLCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLCountryApp.swift
+//  GraphQLCountry
+//
+//  Created by Mattia on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
